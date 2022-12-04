a5c2ba6360bd1335b9d60340cef0d7ec6dd5c1c0
Prakash Kumar gupta <109419191+anantprakashgupta@users.noreply.github.com>
2022-12-04 13:27:52 +0530

    Add files via upload

---
+++ src/components/ColourSelector.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ColourSelector = (props) => {
+  const { config, selectNextBackground } = props;
+  const { background } = config;
+  return (
+    <button
+      className={config.classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
+      {/* label should come here */}
+      {config.label}
+    </button>
+  );
+};
+export default ColourSelector;
